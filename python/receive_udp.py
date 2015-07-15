5ea53306cc5d395a4180a0552933ddf482416037
dtrejod <dtrejod@gmail.com>
2015-07-14 23:09:22 -0400

    Change tab to spaces. Added default IP:PORT. Fixed encoding issue on receieve_udp.
    Changes:
    - Changed tab characters to spaces to conform to PEP8 style guide
    - Added code to use a default of 127.0.0.1 and port 65104 when no
    IP:port are given for send/receive prompts
    - Fixing receiving encoding bug. Upon receiving the message we need to
    decode using UTF-8.

--- python/receive_udp.py
+++ python/receive_udp.py
@@ -11,13 +11,36 @@
 import socket
 
 # Setup connection to other Pi
+# Prompt for IP. Default localhost if null input
 #
-UDP_IP = str(input("Enter the IP of the client machine you will be communicating with. (Ex. '127.0.0.1'): "))
-UDP_PORT = int(input("Enter the PORT you will be communicating over. (Ex. 5005): "))
+UDP_IP = input("Enter the IP of the client machine you will be communicating with. (Default: '127.0.0.1')>> ") or '127.0.0.1'
 
+# Prompt for PORT. Default 65104 if null input
+#   
+while True: 
+    try:
+        UDP_PORT = int(input("Enter the PORT you will be communicating over. (Default: 65104)>> ") or '65104')
+    except ValueError:
+        print("You need to type in a valid PORT number!")
+        continue
+    else:
+        if UDP_PORT in range(65535):
+            break
+        else:
+            print("Port must be in range 0-65535!")
+            continue
+
+# Bind the socket
+#
 sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM) # Receive over internet using UDP
 sock.bind((UDP_IP, UDP_PORT))
 
+# Prompt user we are now monitoring for messages
+#
+print("Program is now monitoring {}:{} for messages" .format(str(UDP_IP), UDP_PORT))
 while True:
+    print ("Waiting for Message")
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    data =  bytes.decode(data, 'UTF-8')
+    
     print ("received message: {}" .format(data))
--- python/send_udp.py
+++ python/send_udp.py
@@ -11,26 +11,43 @@
 import socket
 
 # Setup connection to other Pi
+# Prompt for IP. Default localhost if null input
 #
-UDP_IP = input("Enter the IP of the client machine you will be communicating with. (Ex. '127.0.0.1'): ")
-UDP_PORT = input("Enter the PORT you will be communicating over. (Ex. 5005): ")
+UDP_IP = input("Enter the IP of the client machine you will be communicating with. (Default: '127.0.0.1')>> ") or '127.0.0.1'
+
+# Prompt for PORT. Default 65104 if null input
+#
+while True: 
+    try:
+        UDP_PORT = int(input("Enter the PORT you will be communicating over. (Default. 65104)>> ") or '65104')
+    except ValueError:
+        print("You need to type in a valid PORT number!")
+        continue
+    else:
+        if UDP_PORT in range(65535):
+            break
+        else:
+            print("Port must be in range 0-65535!")
+            continue
+# Bind the socket
+#
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Send over internet using UDP
 
 
 # Run the program forever
 while True:
-	# Prompt the user for a keystroke or message to send
-	#
-	MESSAGE = input("What would you like to send (keyboard input) ")
-	
-	# Let the user know what IP and Port we are using to communicate with
-	#
-	print ('Sending message to UDP target: {}  using port: {}' .format(str(UDP_IP), str(UDP_PORT)))
-	
-	# Send the message using the socket opened
-	#
-	sock.sendto(bytes(MESSAGE, 'UTF-8'), (UDP_IP, UDP_PORT))
-	
-	# Confirm with the user the message sent succesfully
-	#
-	print ("Message sent: {}" .format(str(MESSAGE)))
+    # Prompt the user for a keystroke or message to send
+    #
+    MESSAGE = input("What would you like to send (keyboard input)>> ")
+    
+    # Let the user know what IP and Port we are using to communicate with
+    #
+    print ('Sending message to UDP target: {}:{}' .format(str(UDP_IP), UDP_PORT))
+    
+    # Send the message using the socket opened
+    #
+    sock.sendto(bytes(MESSAGE, 'UTF-8'), (UDP_IP, UDP_PORT))
+    
+    # Confirm with the user the message sent succesfully
+    #
+    print ("Successfully sent message: {}" .format(str(MESSAGE)))
