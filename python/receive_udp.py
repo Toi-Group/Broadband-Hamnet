6e1bef6f202c8236f67b269a5f127bcb9e03c8c0
dtrejod <dtrejod@gmail.com>
2015-07-14 22:06:09 -0400

    send/receive_udp support for user input of IP/PORT DEST and Message
    I have added code to prompt the user for input from/to where they want
    to send their message to. I have also added code to prompt the user for
    what message they wish to send over this communication channel.

--- python/receive_udp.py
+++ python/receive_udp.py
@@ -1,12 +1,23 @@
+# !python3
+#
+
+# Source: https://wiki.python.org/moin/UdpCommunication
+#
+
+# This code receives a message from a UDP_IP source running the script 
+# send_udp.py
+#
+
 import socket
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+# Setup connection to other Pi
+#
+UDP_IP = str(input("Enter the IP of the client machine you will be communicating with. (Ex. '127.0.0.1'): "))
+UDP_PORT = int(input("Enter the PORT you will be communicating over. (Ex. 5005): "))
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM) # Receive over internet using UDP
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    print ("received message: {}" .format(data))
--- python/send_udp.py
+++ python/send_udp.py
@@ -1,13 +1,36 @@
+# !python3
+#
+
+# Source: https://wiki.python.org/moin/UdpCommunication
+#
+
+# This code sends a message to another script running at the UDP_IP destination
+# the script receive_udp.py
+#
+
 import socket
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!"
+# Setup connection to other Pi
+#
+UDP_IP = input("Enter the IP of the client machine you will be communicating with. (Ex. '127.0.0.1'): ")
+UDP_PORT = input("Enter the PORT you will be communicating over. (Ex. 5005): ")
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Send over internet using UDP
 
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+# Run the program forever
+while True:
+	# Prompt the user for a keystroke or message to send
+	#
+	MESSAGE = input("What would you like to send (keyboard input) ")
+	
+	# Let the user know what IP and Port we are using to communicate with
+	#
+	print ('Sending message to UDP target: {}  using port: {}' .format(str(UDP_IP), str(UDP_PORT)))
+	
+	# Send the message using the socket opened
+	#
+	sock.sendto(bytes(MESSAGE, 'UTF-8'), (UDP_IP, UDP_PORT))
+	
+	# Confirm with the user the message sent succesfully
+	#
+	print ("Message sent: {}" .format(str(MESSAGE)))
