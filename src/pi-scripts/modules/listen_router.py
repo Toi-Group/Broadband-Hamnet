639bfa87ae6d31319f8021cd1717d2d690d779f3
Robert Irwin <tue65096@temple.edu>
2016-02-20 22:46:00 +0000

    Automated ssh login is functioning. Need to check for correctoness of password. Also, the file <user_pwd.txt> should be deleted when the program exits.

--- src/pi-scripts/modules/listen_router.py
+++ src/pi-scripts/modules/listen_router.py
@@ -2,24 +2,20 @@
 import os, sys
 import subprocess
 from modules.gatewayIP import gatewayIP
+
+
 def listen_router():
     # Find the default gateway
     # 
     default_gateway = gatewayIP()
-    print (default_gateway)
-    # Find directory with router scripts
-    #
-    # scriptPath = os.path.join(os.path.join( \
-    #    os.path.dirname(os.path.dirname(os.path.dirname( \
-    #    os.path.dirname(os.path.dirname(
-    #    os.path.abspath(__file__)))))), \
-    #    "router-scripts"), \
-    #    'router_tx_arpinfo.sh')
+
     scriptPath = "../etc/router_tx_arpinfo.sh"
+
     # Run '../router-scripts/router_tx_arpinf.sh' on local router
     #
-    ssh = subprocess.Popen(['ssh', '-p', '2222', \
-        'root@' + default_gateway, "'sh '" + scriptPath], \
+    ssh = subprocess.Popen(['sshpass', '-fuser_pwd.txt', \
+        'ssh', '-p', '2222', \
+        'root@' + default_gateway, "sh "  + scriptPath], \
         shell=False, \
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE)
--- src/pi-scripts/modules/txArpInfo.py
+++ src/pi-scripts/modules/txArpInfo.py
@@ -3,9 +3,14 @@ from modules.listen_router import listen_router
 
 def txArpInfo():
 
+    user_pwd = input("Router Password: ")
+
+    with open("user_pwd.txt", "wt") as f:
+        f.write(user_pwd)
+
     # Start a thread to continuously run the listen router script
     #
     TXarp = Thread(target=listen_router)
     TXarp.daemon = True
     TXarp.start()
-    return 1
+
